615a67a6d4c4c2d92237284e0f72a645b68365b6
Antonio Teran <antonioteran@Antonios-MacBook-Pro.local>
2013-09-22 23:31:28 +0200

    adding files

---
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,103 @@
+package com.example.myfirstapp;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        //Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        
+        //Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        
+        //Set the text view as the activity layout
+        setContentView(textView);
+
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+//public class DisplayMessageActivity extends Activity {
+//	
+//	@SuppressLint("NewApi")
+//	@Override
+//	protected void onCreate(Bundle savedInstanceState) {
+//		
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_display_message);
+//		
+//		// Show the Up button in the action bar.
+//		//setupActionBar(); /////////////////////////// CAMBIO?
+//		
+//		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+//            // Show the Up button in the action bar.
+//            getActionBar().setDisplayHomeAsUpEnabled(true);
+//        }
+//		
+//	}//end of the onCreate method
+//
+//	
+//	/**
+//	 * Set up the {@link android.app.ActionBar}, if the API is available.
+//	 */
+//	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+//	private void setupActionBar() {
+//		
+//		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+//			getActionBar().setDisplayHomeAsUpEnabled(true);
+//		}//end of if loop
+//		
+//	}//end of the setupActionBar method
+//
+//	
+//
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		
+//		switch (item.getItemId()) {
+//		case android.R.id.home:
+//			// This ID represents the Home or Up button. In the case of this
+//			// activity, the Up button is shown. Use NavUtils to allow users
+//			// to navigate up one level in the application structure. For
+//			// more details, see the Navigation pattern on Android Design:
+//			//
+//			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+//			//
+//			NavUtils.navigateUpFromSameTask(this);
+//			return true;
+//		}
+//		return super.onOptionsItemSelected(item);
+//		
+//	}//end of the onOptionsItemSelected method
+//
+//}//end of the DisplayMessageActivity class
